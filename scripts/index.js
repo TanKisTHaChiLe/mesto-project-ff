7e46290b2458e084e0aa14c8cf8dc411421700b7
Artem Lanets <lanetsarn@yandex.ru>
2025-03-25 23:02:21 +0300

    Готов к выдаче

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 
+const cardList = document.querySelector('.places__list');
+
 // @todo: Функция создания карточки
+
+function addCard(cardTitle, cardLink, removeCard) {
+    const cardElemnet = cardTemplate.querySelector('.places__item').cloneNode(true);   
+    cardElemnet.querySelector('.card__image').setAttribute('src', cardLink);
+    cardElemnet.querySelector('.card__title').textContent = cardTitle;
+    cardElemnet.querySelector('.card__delete-button').addEventListener('click', () => {removeCard(cardElemnet)});
+    cardList.append(cardElemnet);
+} 
 
 // @todo: Функция удаления карточки
 
+function removeCard(element) {
+    element.remove();
+}
+
 // @todo: Вывести карточки на страницу
+initialCards.forEach(item => {
+    addCard(item.name,item.link, removeCard);
+})
+
